e633f79fe654840821ff67ea481f5b55b7d4dacd
qizl <q@qizl.cn>
2017-05-04 23:43:47 +0800

    -p

---
+++ MS Win32 API Practice/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MS_Win32_API_Practice
+{
+    class Program
+    {
+        [DllImport("User32.dll")]
+        public static extern int MessageBox(int h, string m, string c, int type);
+
+        static void Main(string[] args)
+        {
+            MessageBox(0, "Hello Win32 API", "qizl", 4);
+
+            //System.Windows.Forms.Cursor.Clip = new Rectangle(1, 1, 1, 1);
+            Console.Read();
+        }
+    }
+}
